ac59cc4a243ed5113551ea211e586e115a048a6e
shot <shot131@gmail.com>
2019-12-17 17:08:43 +0300

    Added $SITE_NAME to /etc/hosts

--- vagrant/on_up.sh
+++ vagrant/on_up.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sed -i -e "s/^127.0.1.1\t$SITE_URL.*$/$HOST_IP www.$SITE_URL $SITE_URL/g" /etc/hosts
+sed -i -e "s/^127.0.1.1\t$SITE_URL.*$/$HOST_IP www.$SITE_URL $SITE_URL $SITE_NAME/g" /etc/hosts
 sed -i -e "s/^export APACHE_RUN_USER.*$/export APACHE_RUN_USER=vagrant/g" -e "s/^export APACHE_RUN_GROUP.*$/export APACHE_RUN_GROUP=vagrant/g" /etc/apache2/envvars
 sed -e "s/SITE_NAME/$SITE_URL/g" -e "s/HOST_IP/$HOST_IP/g" /vagrant/vagrant/provision/apache/vhost.conf > "/etc/apache2/sites-enabled/$SITE_URL.conf"
 cp /vagrant/vagrant/provision/php/user.ini /etc/php/7.1/apache2/conf.d/user.ini
